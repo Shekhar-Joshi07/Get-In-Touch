e30b3467823878b72c45c8f5e7358e4abafee4f8
Shekhar-Joshi07 <shekharjoshi64320@gmail.com>
2023-06-02 10:40:55 +0530

    backend completed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -38,6 +38,16 @@ export const getFeedPosts = async (req, res) => {
   }
 };
 
+export const getUserPosts = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const post = await Post.find({ userId });
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+
 /* UPDATE */
 
 export const likePost = async (req, res) => {
