012a0f1cf5f602c48625f1a32ba6334c11bbbfc4
Shekhar-Joshi07 <shekharjoshi64320@gmail.com>
2023-06-03 21:01:51 +0530

    fixed a bug related to logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
@@ -12,18 +11,25 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
-     <BrowserRouter>
-     <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
           <CssBaseline />
-     <Routes>
-      <Route path="/" element={<LoginPage/>}/>
-      <Route path="/home" element={<HomePage/>}/>
-      <Route path="/profile/:userId" element={<ProfilePage/>}/>
-     </Routes>
-     </ThemeProvider>
-     </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
