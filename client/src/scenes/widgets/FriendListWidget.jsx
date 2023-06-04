566f9446e73db4b0773ab0251c9614a3925170c0
Shekhar-Joshi07 <shekharjoshi64320@gmail.com>
2023-06-04 13:55:39 +0530

    friend list widget also added

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -25,7 +25,7 @@ const FriendListWidget = ({ userId }) => {
 
   useEffect(() => {
     getFriends();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps 
 
   return (
     <WidgetWrapper>
