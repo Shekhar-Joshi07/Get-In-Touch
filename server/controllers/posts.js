394619c75b9d43daa7d64e9de578ce0c10241c8f
Shekhar-Joshi07 <shekharjoshi64320@gmail.com>
2023-06-06 10:39:19 +0530

    deployment setup

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,13 +2,12 @@ import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 /* CREATE */
-
 export const createPost = async (req, res) => {
   try {
-    const { userID, description, picturePath } = req.body;
-    const user = await User.findById(userID);
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
     const newPost = new Post({
-      userID,
+      userId,
       firstName: user.firstName,
       lastName: user.lastName,
       location: user.location,
@@ -19,37 +18,75 @@ export const createPost = async (req, res) => {
       comments: [],
     });
     await newPost.save();
+
     const post = await Post.find();
     res.status(201).json(post);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
+  } catch (err) {
+    res.status(409).json({ message: err.message });
   }
 };
-
-/* READ */
-
-export const getFeedPosts = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const post = await Post.find({userId});
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
-
-export const getUserPosts = async (req, res) => {
-    try {
-      const { userId } = req.params;
-      const post = await Post.find({ userId });
-      res.status(200).json(post);
-    } catch (err) {
-      res.status(404).json({ message: err.message });
-    }
-  };
 
 /* UPDATE */
+export const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description, picturePath } = req.body;
 
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { description, picturePath },
+      { new: true }
+    );
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/* DELETE */
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedPost = await Post.findByIdAndDelete(id);
+
+    if (!deletedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate("comments.userId");
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const posts = await Post.find({ userId }).populate("comments.userId");
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+
+/* UPDATE */
 export const likePost = async (req, res) => {
   try {
     const { id } = req.params;
@@ -62,13 +99,45 @@ export const likePost = async (req, res) => {
     } else {
       post.likes.set(userId, true);
     }
+
     const updatedPost = await Post.findByIdAndUpdate(
       id,
       { likes: post.likes },
       { new: true }
     );
+
     res.status(200).json(updatedPost);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+
+export const addComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId, comment } = req.body;
+
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const newComment = {
+      userId,
+      comment,
+    };
+
+    post.comments.push(newComment);
+    await post.save();
+
+    // Populate the user information in the comment
+    await post
+      .populate({ path: "comments.userId", model: "User" })
+      
+  
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,18 +1,19 @@
-import express from "express"
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js"
-import { verifyToken } from "../middleware/auth.js"
-
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost, addComment, deletePost, updatePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-
 /* READ */
-
-router.get("/",verifyToken,getFeedPosts);
+router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
 
-
 /* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/comment", verifyToken, addComment);
+router.patch("/:id", verifyToken, updatePost);
 
-router.patch("/:id/like", verifyToken, likePost );
+/* DELETE */
+router.delete("/:id", verifyToken, deletePost);
+
 export default router;
